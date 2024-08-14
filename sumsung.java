9a15dde041165f480f4370bf8e006166f33713e0
oukhqjha1151 <okkuiwah@outlook.com>
2024-08-14 18:28:08 +0800

    Create sumsung.java

---
+++ sumsung.java
@@ -0,0 +1,54 @@
+------SubClass 类继承------
+SuperClass()
+SubClass()
+SuperClass(int n)
+SubClass(int n): 100
+------SubClass2 类继承------
+SuperClass(int n)
+SubClass2()
+SuperClass()
+SubClass2(int n): 200
+  /* 文件名 : Employee.java */
+public abstract class Employee
+{
+   private String name;
+   private String address;
+   private int number;
+   public Employee(String name, String address, int number)
+   {
+      System.out.println("Constructing an Employee");
+      this.name = name;
+      this.address = address;
+      this.number = number;
+   }
+   public double computePay()
+   {
+     System.out.println("Inside Employee computePay");
+     return 0.0;
+   }
+   public void mailCheck()
+   {
+      System.out.println("Mailing a check to " + this.name
+       + " " + this.address);
+   }
+   public String toString()
+   {
+      return name + " " + address + " " + number;
+   }
+   public String getName()
+   {
+      return name;
+   }
+   public String getAddress()
+   {
+      return address;
+   }
+   public void setAddress(String newAddress)
+   {
+      address = newAddress;
+   }
+   public int getNumber()
+   {
+     return number;
+   }
+}
